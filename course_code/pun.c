af464e56e3e0030f66fd7252fec5ff8eced321ad
GideonMissoy <missoykipsang@gmail.com>
2023-02-08 23:52:36 -0800

    This function prints a bad pun.

--- course_code/pun.c
+++ course_code/pun.c
@@ -1,7 +1,14 @@
+/**
+ * main - prints a bad pun
+ * Name: pun.c
+ * Author: K. N. King
+ * Return: void
+ */
+
 #include <stdio.h>
 
 int main(void)
 {
 	printf("To C, or not to C: that is the question.\n");
-	return 0;
+	return (0);
 }
