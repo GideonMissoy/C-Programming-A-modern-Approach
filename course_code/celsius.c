a6632bcb2aa216c04283ffcec60d45066a10c8c9
GideonMissoy <missoykipsang@gmail.com>
2023-02-09 01:10:14 -0800

    This function converts a Fahrenheit temperature to celsius.

--- course_code/celsius.c
+++ course_code/celsius.c
@@ -1,12 +1,14 @@
 /**
  * main - converts a Fahrenheit temp to celsius
  * Return: temp in celsius
+ *
+ * Defining SCALE_FACTOR to be (5.0f / 9.0f) instead of (5 / 9)
+ * is important, because C turncates the result when two ints are divided.
+ * The value (5 / 9) would be 0, which defn isn't what we want.
  */
-
 #include <stdio.h>
 #define FREEZING_PT 32.0f
 #define SCALE_FACTOR (5.0f / 9.0f)
-
 int main(void)
 {
 	float fahrenheit, celsius;
